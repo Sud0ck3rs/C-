da8dfb18f63193c1cf23bb5f7462d9d86d60eb91
Sud0ck3rs <98493462+Sud0ck3rs@users.noreply.github.com>
2022-03-23 19:56:02 +0100

    Update subprocess.hpp

--- subprocess.hpp
+++ subprocess.hpp
@@ -7,6 +7,10 @@
 #include <cstdlib>
 #include <cstdio>
 #include <array>
+#include <windows.h>
+#include <process.h>
+#include <Tlhelp32.h>
+#include <winbase.h>
 
 namespace subprocess
 {
@@ -45,6 +49,29 @@ namespace subprocess
                 return result_Shell;
             }
 
+            void killProcessByName(const char *filename)
+            {
+                HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+                PROCESSENTRY32 pEntry;
+                pEntry.dwSize = sizeof (pEntry);
+                BOOL hRes = Process32First(hSnapShot, &pEntry);
+                while (hRes)
+                {
+                    if (strcmp(pEntry.szExeFile, filename) == 0)
+                    {
+                        HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0,
+                                                    (DWORD) pEntry.th32ProcessID);
+                        if (hProcess != NULL)
+                        {
+                            TerminateProcess(hProcess, 9);
+                            CloseHandle(hProcess);
+                        }
+                    }
+                    hRes = Process32Next(hSnapShot, &pEntry);
+                }
+                CloseHandle(hSnapShot);
+            }
+
         private:
             std::string cmd_Shell;
             std::string result_Shell;
@@ -53,5 +80,4 @@ namespace subprocess
             FILE *pipe;
     };
 }
-
 #endif
